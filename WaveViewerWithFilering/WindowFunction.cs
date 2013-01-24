8db7cd27b59bf0b67db38961009d0a1bec4f1be1
Atsushi TANABE <tanabe.atsushi@nikken.jp>
2013-01-24 23:11:59 +0900

    窓関数を追加

---
+++ WaveViewerWithFilering/WindowFunction.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WaveViewerWithFilering
+{
+    class WindowFunction
+    {
+        private double[] factor_; public double[] factor { get { return factor_; } }
+        private int n_; public int n { get { return n_; } }
+
+        public WindowFunction(int n)
+        {
+            n_ = n;
+            factor_ = new  double[n+1];
+        }
+
+        public double this[int k]
+        {
+            get {
+                if (Math.Abs(k) > n)
+                    return 0.0;
+                return factor[Math.Abs(k)]; 
+            }
+        }
+    }
+
+    class RectangleWindow : WindowFunction
+    {
+        public RectangleWindow(int n)
+            : base(n)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                factor[i] = 1.0;
+            }
+        }
+    }
+
+    class HannWindow : WindowFunction
+    {
+        public HannWindow(int n)
+            : base(n)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                double x = 0.5 + (0.5 * i) / n;
+                factor[i] = 0.5 - 0.5 * Math.Cos(2 * Math.PI * x);
+            }
+        }
+    }
+
+    class HanningWindow : HannWindow
+    {
+        public HanningWindow(int n) : base(n) { }
+    }
+
+    class HammingWindow : WindowFunction
+    {
+        public HammingWindow(int n)
+            : base(n)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                double x = 0.5+(0.5*i) / n;
+                factor[i] = 0.54 - 0.46 * Math.Cos(2 * Math.PI * x);
+            }
+        }
+    }
+
+    class BlackmanWindow : WindowFunction
+    {
+        public BlackmanWindow(int n)
+            : base(n)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                double x = 0.5 + (0.5 * i) / n;
+                factor[i] = 0.42 - 0.5 * Math.Cos(2 * Math.PI * x) + 0.08 * Math.Cos(4 * Math.PI * x);
+            }
+        }
+    }
+
+
+    class KaiserWindow : WindowFunction
+    {
+        public KaiserWindow(int n, double alpha)
+            : base(n)
+        {
+            double z = bessi0(Math.PI * alpha);
+            for (int i = 0; i <= n; i++)
+            {
+                double x = 0.5 + (0.5 * i) / n;
+                double y = Math.PI * alpha * Math.Sqrt(1 - Math.Pow(2 * x - 1, 2));
+                factor[i] = bessi0(y) / z;
+            }
+        }
+
+
+        /// <summary>
+        /// Modified bessel funcion I0
+        /// 
+        ///  From Numerical Recipes in C pp. 187-189. (source: p 189)
+        /// </summary>
+        /// <param name="x">arbitrary real number</param>
+        /// <returns>function value</returns>
+        static double bessi0(double x)
+        {
+            double ax = Math.Abs(x);
+            double y;
+            if( ax < 3.75 )
+            {
+                y = x / 3.75;
+                y *= y;
+                return 1.0+y*(3.5156229+y*(3.0899424+y*(1.2067492
+                    +y*(0.2659732+y*(0.0360768+y*0.45813E-2)))));
+            }else{
+                y = 3.75 / ax;
+                return (Math.Exp(ax) / Math.Sqrt(ax)) * (0.39894228 + y * (0.1328592e-1
+                    + y * (0.225319e-2 + y * (-0.157565e-2 + y * (0.916281e-2
+                    + y * (-0.2057706e-1 + y * (0.2635537e-1 + y * (-0.1647633e-1
+                    + y * 0.392377e-2))))))));
+            }
+        }
+    }
+
+
+}
